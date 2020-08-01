72b1bb1933573a0d1b04e10b081c100179bb62cd
Giovani Boff <giovanisouza15@gmail.com>
2020-08-01 15:08:12 -0300

    adiciona SessionController

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 import routes from "./router";
 class App {
   constructor() {
     this.server = express();
+    mongoose.connect(
+      "mongodb+srv://devhouse:devhouse@devhouseapicluster.ic5wv.mongodb.net/devhouse?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     this.middlewares();
     this.routes();
   }
